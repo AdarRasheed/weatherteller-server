7c4fd8d04a02acd4dda16f91469074e5098b30a2
Adar Rasheed <adar.rasheed@gmail.com>
2021-10-09 02:20:25 +0500

    basic router setup to fetch weather data

---
+++ src/main/java/com/assignmento/weathertellerserver/routes/WeatherFetchRouter.java
@@ -0,0 +1,23 @@
+package com.assignmento.weathertellerserver.routes;
+
+import com.assignmento.weathertellerserver.utils.CustomLoggerUtil;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WeatherFetchRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        String endPoint = "weather://weather-fetcher"
+                            + "?location=" + "Madrid,Spain"
+                            + "&delay=" + "1000"
+                            + "&appid=" + "aa67371fffed03e5964428e6c7d83927";
+        from(endPoint)
+//                .log("${body}")
+                .to("log:weather-fetcher");
+    }
+
+}
