3a637f58f52ccca9a2abd3a4f8155f36b010e7c4
Adar Rasheed <adar.rasheed@gmail.com>
2021-10-08 22:27:31 +0500

    initial project setup with Devtools, Web and Apache Camel

---
+++ src/main/java/com/assignmento/weathertellerserver/WeathertellerServerApplication.java
@@ -0,0 +1,13 @@
+package com.assignmento.weathertellerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathertellerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathertellerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignmento/weathertellerserver/WeathertellerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignmento.weathertellerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathertellerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
